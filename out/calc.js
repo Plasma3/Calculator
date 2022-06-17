8dea083025ff9de49e91f9e568398bf53def456d
Fredrik Ahlberg <43331256+Plasma3@users.noreply.github.com>
2022-06-17 02:31:04 +0200

    played around with stashes

--- out/calc.js
+++ out/calc.js
@@ -4,5 +4,4 @@ var mathclasses_1 = require("./mathclasses");
 // let foo = new expression([new nodeNumber(2)])
 var foo = new mathclasses_1.expression([new mathclasses_1.nodeNumber(2)]);
 console.log(foo);
-console.log();
 //# sourceMappingURL=calc.js.map
--- src/backend/calc.ts
+++ src/backend/calc.ts
@@ -5,8 +5,3 @@ import { expression, nodeNumber } from "./mathclasses";
 let foo = new expression([new nodeNumber(2)])
 
 console.log(foo)
-
-
-
-
-console.log()
