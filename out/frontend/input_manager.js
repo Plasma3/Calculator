2b95b49879f8708ff57b1e17f17dadee51bb09ab
Fredrik Ahlberg <43331256+Plasma3@users.noreply.github.com>
2022-06-24 00:50:19 +0200

    made input interactable

---
+++ out/frontend/input_manager.js
@@ -0,0 +1,14 @@
+"use strict";
+var app = document.getElementById("app");
+var input = document.createElement("input");
+input.id = "input";
+input.type = "text";
+app === null || app === void 0 ? void 0 : app.appendChild(input);
+input.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        input.blur();
+        console.log(input.value);
+    }
+});
+//# sourceMappingURL=input_manager.js.map
